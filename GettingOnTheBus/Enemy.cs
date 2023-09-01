7071fef3c8d4b92753b40e282fa50f737acaffe8
Bruno <b.b.da.costa@gmail.com>
2023-09-01 10:09:48 -0300

    ♻️ Adição de tarefas

--- GettingOnTheBus/Enemy.cs
+++ GettingOnTheBus/Enemy.cs
@@ -9,27 +9,30 @@ namespace GettingOnTheBus
     public class Enemy : ISubscribable
     {
         private readonly int enemyNumber;
+
+        // TODO: EnemySpeedManager, referencias aos inimigos influenciados, singleton
+        // criar um manager de velocidade que irá alterar a velocidade de cada inimigo registrado
+        // criar uma classe abstrata do tipo singleton que o EnemySpeedManager irá herdar
         private readonly int regularSpeed = 1;
 
-        public float Speed {get; set;}
-        
-        public Enemy (int enemyNumber)
+        public float Speed { get; set; }
+
+        public Enemy(int enemyNumber)
         {
             this.enemyNumber = enemyNumber;
             this.Speed = 0;
+            // exemplo de interface: EnemySpeedManager.Instance.Register(this);
         }
 
         public void Handle(IEvent currentEvent)
         {
-            if (currentEvent.EventType == "SpeedItem")
-            {
+            if(currentEvent.EventType == "SpeedItem")
                 UpdateSpeed(currentEvent.GetData());
-            }
         }
 
         public void UpdateSpeed(ISpeedChanger speedChanger)
         {
-            if (speedChanger.SpeedCoeff == 0)
+            if(speedChanger.SpeedCoeff == 0)
             {
                 Speed = regularSpeed;
             }
---
+++ GettingOnTheBus/EventBusTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingOnTheBus
+{
+    public class EventBusTests
+    {
+        [Test, Ignore("not implemented")]
+        public void Should_pass()
+        {
+
+        }
+    }
+}
